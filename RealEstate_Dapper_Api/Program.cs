0c255067522270e417be13a3396cd7c97e97564d
Tural Hasanov <tural_hasanov@bk.ru>
2023-09-15 14:49:27 +0400

    get categorylist api

--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -1,6 +1,11 @@
+using RealEstate_Dapper_Api.Models.DapperContext;
+using RealEstate_Dapper_Api.Models.Repositories.CategoryRepositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<Context>();
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
