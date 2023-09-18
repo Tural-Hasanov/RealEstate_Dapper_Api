452dd99aaa329555cf711d313d1d7eba841cb55e
Tural Hasanov <tural_hasanov@bk.ru>
2023-09-18 11:34:23 +0400

    update component partials

---
+++ RealEstate_Dapper_Api/Models/Repositories/ProductRepository/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.ProductDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Models.Repositories.ProductRepository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly Context _context;
+
+        public ProductRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultProductDto>> GetAllProductAsync()
+        {
+            string query = "Select * from Product";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultProductDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync()
+        {
+            string query = "select * from Product inner join Category on Product.ProductCategory=Category.CategoryID";
+            using (var connections = _context.CreateConnection())
+            {
+                var values = await connections.QueryAsync<ResultProductWithCategoryDto>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
